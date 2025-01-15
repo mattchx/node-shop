d2016ac61fc26cfc138cdf524bcc671c2423dd76
Matt Henderson <mattcharlesh@gmail.com>
2025-01-15 12:35:37 -0500

    fetch and render product list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+  const [cart, setCart] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  // [] fetch get product list
+  // [] store product list in state
+  // [] create state for cart
+  // [] fetch post the selected item to cart
+
+  const url = 'http://localhost:3001/'
+
+  const fetchProducts = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url + 'products', { method: "GET" });
+      const data = await response.json()
+      setProducts(data)
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    // fetch get products
+    fetchProducts()
+  }, [])
+
+  const addToCart = (id) => {
+    // fetch post id to add item to cart in backend
+    console.log(`Product ID:${id} was added to your cart`)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Welcome to your store</h3>
+      {loading ? "Loading..." :
+        <div className='product-list'>
+          {Array.isArray(products) && products.map(p => (
+            <div className="product" key={p.id}>
+              <strong>{p.name}</strong>
+              <p>{p.price}</p>
+              <button onClick={() => addToCart(p.id)}>Add To Cart</button>
+            </div>
+          ))
+          }
+        </div>
+      }
+
     </>
   )
 }
